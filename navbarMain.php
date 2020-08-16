2a100c0e6bd0870bda4149eda523280fd423d463
Saulg96 <41869878+Saulg96@users.noreply.github.com>
2020-08-16 16:32:22 -0600

    [ADD] navNarMain.php y catalogo.php

---
+++ navbarMain.php
@@ -0,0 +1,102 @@
+<!--Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Provedim</title>
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all">
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+ <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+<!-- Custom Theme files -->
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
+<!-- Custom Theme files -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Shoplist Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template,
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!--Google Fonts-->
+<link href='//fonts.googleapis.com/css?family=Hind:400,500,300,600,700' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Oswald:400,700,300' rel='stylesheet' type='text/css'>
+<!-- start-smoth-scrolling -->
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+	<script type="text/javascript">
+			jQuery(document).ready(function($) {
+				$(".scroll").click(function(event){
+					event.preventDefault();
+					$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+				});
+			});
+	</script>
+<!-- //end-smoth-scrolling -->
+<script src="js/simpleCart.min.js"> </script>
+<script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="header">
+	<div class="container">
+		<div class="header-main">
+			<div class="top-nav">
+				<div class="content white">
+	              <nav class="navbar navbar-default " role="navigation">
+					    <div class="navbar-header">
+					        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						        <span class="sr-only">Toggle navigation</span>
+						        <span class="icon-bar"></span>
+						        <span class="icon-bar"></span>
+						        <span class="icon-bar"></span>
+					        </button>
+					        <div class="navbar-brand logo">
+								<a href="index.php"><img src="logo/logo2.jpeg" alt="" id="logo"></a>
+							</div>
+					    </div>
+					    <!--/.navbar-header-->
+					 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					        <ul class="nav navbar-nav">
+
+					        	   <li><a href="index.php"><b>Home</b></a></li>
+					        	   <li><a href="index.php#quienes-somos"><b>¿Quiénes Somos?</b></a></li>
+					        	   <li><a href="index.php#catalogo"><b>Catálogo</b></a></li>
+
+					        	   <li><a href="index.php#cotizaciones"><b>Cotizaciones</b></a></li>
+						        	<li><a href="index.php#proveedores"><b>Proveedores</b></a></li>
+
+
+					        </ul>
+					    </div>
+					    <!--/.navbar-collapse-->
+					</nav>
+					<!--/.navbar-->
+				</div>
+			</div>
+			<div class="header-right">
+				<div class="search">
+					<div class="search-text">
+					    <input class="serch" type="text" value="Buscar" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Buscar';}"/>
+					</div>
+					<div class="cart box_1">
+						<a href="checkout.html">
+						<h3>
+							<img src="images/cart.png" alt=""/>
+							<div class="total">
+							<span class="simpleCart_total"></span></div>
+							</h3>
+						</a>
+						<p><a href="javascript:;" class="simpleCart_empty">Empty Cart</a></p>
+					</div>
+					<div class="head-signin">
+						<h5><a href="login.html"><i class="hd-dign"></i>Iniciar Sesión</a></h5>
+					</div>
+                     <div class="clearfix"> </div>
+				</div>
+			</div>
+		 <div class="clearfix"> </div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
