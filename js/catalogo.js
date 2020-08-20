dfa9074286a6f82c7558ba3436b741f18d0450f3
keab16 <38601378+keab16@users.noreply.github.com>
2020-08-19 22:01:18 -0600

    Modificado de Producto a Catalogo

--- js/catalogo.js
+++ js/catalogo.js
@@ -351,7 +351,7 @@ $(document).ready(function(){
 		success:function(respuesta){
 			console.log(respuesta);
 			for (var i = 0; i<respuesta.length; i++) {
-                $("#obtenerProductos").append('<div class="col-md-9 product-block"><div class="col-md-4 home-grid"><div class="home-product-main"><div class="home-product-top"><a href="single.html"><img src="images/h5.jpg" alt="" class="img-responsive zoom-img"></a></div><div class="home-product-bottom"><h3><a href="single.html">'+respuesta[i].nombre+'</a></h3><p>'+respuesta[i].descripcion+'</p></div><div class="srch"><span>$'+respuesta[i].precio+'</span></div></div></div><div class="clearfix"> </div></div>');
+                $("#obtenerProductos").append('<div class="col-md-4 product-block"><div class="col-md-12 home-grid"><div class="home-product-main"><div class="home-product-top"><a href="single.html"><img src="images/h5.jpg" alt="" class="img-responsive zoom-img"></a></div><div class="home-product-bottom"><h3><a href="single.html">'+respuesta[i].nombre+'</a></h3><p>'+respuesta[i].descripcion+'</p></div><div class="srch"><span>Lps. '+respuesta[i].precio+'</span></div></div></div><div class="clearfix"> </div></div>');
 			}
 		}
 	});
